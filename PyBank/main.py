dcee5444cdbc5975c6475c0dd86f3b84f9ca4ded
austin3216 <aroyster@pm.me>
2021-03-31 13:00:25 -0600

    final PyBank code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,41 +1,74 @@
-# Bring in the CSV file contents
+# Bring in the modules
 
 import os
 import csv
+import sys
 
-bank_path = os.path.join('GitHub', 'python-challenge', 'PyBank', 'Resources', 'Budget_data.csv')
+# Connect to csv file
 
-columns = []
-rows = []
+bank_path = os.path.join('PyBank/Resources/Budget_data.csv')
+
+# Create variables
+
+months_total = 0
+month_change_list = []
+net_change_list_month = []
+total_net = 0
+
+greatest_increase = ["",0]
+greatest_decrease = ["",9999999]
+
+
+# Read data from csv file
 
 with open(bank_path) as csvfile:
 
     bank_reader = csv.reader(csvfile, delimiter=',')
-    columns = next(bank_reader)
+    next(bank_reader)
 
-    # Get number of months in dataset 
-    
-    months = list(bank_reader)
-    month_count = len(months)
-    print(month_count)
+    # establish first row, values for total months, net, and previous net to calculate average net
+    first_row = next(bank_reader)
+    months_total += 1
+    total_net += int(first_row[1])
+    prev_net = int(first_row[1])
 
-    # Get net total amount of Profit/Losses
-
-    total = 0
-
+    # loop to read all rows
     for row in bank_reader:
-        rows.append(row)
+
+        # calculate month count
+        months_total += 1
         
-        profit_loss = int(row[1])
-        total += int(profit_loss)
-        print(total)
+        # calculate total profit/loss
+        total_net += int(row[1])
 
+        # values for month-to-month changes to calculate average change
+        net_change = int(row[1]) - prev_net
+        prev_net = int(row[1])
+        net_change_list_month += [net_change]
+        month_change_list += [row[0]]
 
+        # calculate average change
+        avg_chg = round(sum(net_change_list_month)/len(net_change_list_month),2)
 
+        # conditions for greatest increase/decrease
+        if net_change > greatest_increase[1]:
+            greatest_increase[0] = row[0]
+            greatest_increase[1] = net_change
 
+        if net_change < greatest_decrease[1]:
+            greatest_decrease[0] = row[0]
+            greatest_decrease[1] = net_change
 
+# Print final results
 
+results = (f" Total Months: {months_total} \n Total Profit/Loss: ${total_net} \n Average Change: ${avg_chg} \n Greatest Increase in Profits: {greatest_increase} \n Greatest Decrease in Profits: {greatest_decrease}")
 
+print("Financial Analysis")
+print("-----------------------------------------------------------------")
+print(results)
 
+sys.stdout = open('FinAnalysis.txt', 'w')
 
-
+print("Financial Analysis")
+print("-----------------------------------------------------------------")
+print(results)
