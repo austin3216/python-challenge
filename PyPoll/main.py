19ce932a07f982f370f9e835fa7a14e9590fe5ee
austin3216 <aroyster@pm.me>
2021-03-31 14:39:13 -0600

    commented rest of solution

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,6 +2,7 @@
 
 import os
 import csv
+import sys
 
 # Connect to csv file
 
@@ -10,10 +11,8 @@ csv_path = os.path.join('PyPoll/Resources/Election_data.csv')
 # Create variables
 
 total_votes = 0
-candidates = []
-pct_votes_cand = ["", 0]
-tot_votes_cand = ["", 0]
-winner = ""
+winner_votes = 0
+candidates = {}
 
 # Read csv file
 
@@ -22,13 +21,38 @@ with open(csv_path) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=',')
     next(csv_reader)
 
+    # set the first row of data
     first_row = next(csv_reader)
     total_votes += 1
 
+    # loop through the rows
     for row in csv_reader:
+
+        # calculate total votes
         total_votes += 1
+        
+        # get number of times/rows candidate name repeated (votes)
+        candidates[row[2]] = candidates.get(row[2], 0) + 1
 
-print(total_votes)
+# Need another loop to isolate candidate data...
+    
+    # calculate percentage of votes each candidate
+    pct_vote = # need to format as percent... some variable for candidate votes / total_votes * 100
+    
+    # condition for winner
+    if # candidate votes > winner_votes:
+        # candidate votes = winner_votes
+        winner = # candidate from list
 
+# Print results
 
+print("Election Results")
+print("--------------------------------------")
+print(f"Total Votes: {total_votes}")
+print("--------------------------------------")
+# print(candidate list with percent and total votes) - do this in or outside of loop?
+print("--------------------------------------")
+print(f"Winner: {winner}")
+print("--------------------------------------")
 
+# sys.stdout = open('PollResults.txt', 'w')
